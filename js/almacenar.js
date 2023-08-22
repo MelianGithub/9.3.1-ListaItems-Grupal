e268bc4905a0a4f5740ef1c1aea41f5dd4f5d045
MelianGithub <e_melian04@hotmail.com>
2023-08-22 06:43:15 -0300

    arreglando funcionalidad

--- js/almacenar.js
+++ js/almacenar.js
@@ -1,1 +1,37 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    const agregarItem = document.getElementById('agregar');
+    const contenedor = document.getElementById('contenedor');
+    const limpiar = document.getElementById('limpiar');
+
+    let items = JSON.parse(localStorage.getItem('listado'));
+
+    const actualizarDatosContenedor = () => {
+        contenedor.innerHTML = '';
+
+        items.forEach(item => {
+            const li = document.createElement('li')
+            li.textContent = item;
+            contenedor.appendChild(li);
+        });
+    }
+    actualizarDatosContenedor();
+
+    agregarItem.addEventListener('click', () => {
+
+        const item = document.getElementById('item').value;
+        items.push(item)
+        localStorage.setItem('listado', JSON.stringify(items))
+
+
+        actualizarDatosContenedor();
+    })
+
+    limpiar.addEventListener('click', () => {
+        localStorage.removeItem('listado');
+        items = []; 
+        actualizarDatosContenedor(); 
+    });
+});
+
